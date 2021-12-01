f0efc37cd467dedde967179d8b4cc4523528434e
Adetola Agunbiade <alex4aws@outlook.com>
2021-11-30 19:09:02 -0500

    create pod and container using a deployment

--- posts/index.js
+++ posts/index.js
@@ -1,22 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
 //import router
-const postsRouters = require('./routers/postsRouter');
-
+const postsRouters = require("./routers/postsRouter");
 
 //use middlewares
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(postsRouters)
-
-
+app.use(postsRouters);
 
 const PORT = 8001;
 app.listen(PORT, () => {
-    console.log(`Posts server started on port ${PORT}`)
-})
+  console.log("v55");
+  console.log(`Posts server started on port ${PORT}`);
+});
 /**
  * using bodyParser
  * instead of using 'express.json(), we can follow the steps below to use bodyParser.
