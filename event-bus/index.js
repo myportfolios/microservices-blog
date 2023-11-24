3fd013717d37672599bc0e1a3ffe477cef02393e
Adetola Alexander Agunbiade <30411005+myportfolios@users.noreply.github.com>
2023-11-23 19:47:50 -0500

    1. added comments and formatting. 2. At this point, posts Service (posts-service) works and I am able to access posts endpoint using NodePort)

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,45 +1,45 @@
-const express = require("express");
-const bodyParser = express.json();
-const axios = require("axios");
+const express = require('express')
+const bodyParser = express.json()
+const axios = require('axios')
 
 //instantiate app
-const app = express();
+const app = express()
 //use middlewares
-app.use(bodyParser);
-//empty array to take in the list of events recived from all the services
-const events = [];
+app.use(bodyParser)
+//empty array to take in the list of events received from all the services
+const events = []
 
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   //The event bus will make a POST call to the fllg urls with the 'event' data
 
   // whatever was sent in the request body (e.g,json datatype) will be the event
-  const event = req.body;
+  const event = req.body
   //push the received event into the array variable - events
-  events.push(event);
-  console.log("event", event);
-  axios.post("http://localhost:8000/events", event).catch((e) => {
+  events.push(event)
+  console.log('event', event)
+  axios.post('http://localhost:8000/events', event).catch((e) => {
     //query service
-    console.log(e.message);
-  });
-  axios.post("http://localhost:8001/events", event).catch((e) => {
-    console.log(e.message);
-  }); //posts service endpoint
-  axios.post("http://localhost:8002/events", event).catch((e) => {
-    console.log(e.message);
-  }); //comments service endpoint
-  axios.post("http://localhost:8003/events", event).catch((e) => {
-    console.log(e.message);
-  }); //moderation service
+    console.log(e.message)
+  })
+  axios.post('http://localhost:8001/events', event).catch((e) => {
+    console.log(e.message)
+  }) //posts service endpoint
+  axios.post('http://localhost:8002/events', event).catch((e) => {
+    console.log(e.message)
+  }) //comments service endpoint
+  axios.post('http://localhost:8003/events', event).catch((e) => {
+    console.log(e.message)
+  }) //moderation service
 
-  res.send({ status: "OK" });
-});
+  res.send({ status: 'OK' })
+})
 
-//GET- route to get all the list of events recived by event-bus
-app.get("/events", (req, res) => {
+//GET- route to get all the list of events received by event-bus
+app.get('/events', (req, res) => {
   //send the list of events
-  res.send(events);
-});
-app.listen(4005, () => console.log("Event bus listening on port 4005"));
+  res.send(events)
+})
+app.listen(4005, () => console.log('Event bus listening on port 4005'))
 
 /**
  **** Notes ******
--- query/util/utils.js
+++ query/util/utils.js
@@ -1,33 +1,33 @@
-const posts = require("../store/postsWithCommentsDB");
+const posts = require('../store/postsWithCommentsDB')
 /**
  * @param function handleEvent(type, data) {}
  * a function that handles all the logic of constructing a 'post' obj based on the type
  * of comment.
- * The 'post' si then sved in the store
+ * The 'post' is then sved in the store
  */
 exports.handleEvent = (type, data) => {
-  if (type === "PostCreated") {
-    const { id, title } = data;
-    posts[id] = { id, title, comments: [] }; //adds the id & title on the 'posts' obj imported from store
+  if (type === 'PostCreated') {
+    const { id, title } = data
+    posts[id] = { id, title, comments: [] } //adds the id & title on the 'posts' obj imported from store
     //add a 'comments' variable defaulted to an empty array. it will contain the comments created by this user
   }
-  if (type === "CommentCreated") {
-    const { id, content, postId, status } = data;
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data
     //get actual pos from db
-    const post = posts[postId];
+    const post = posts[postId]
     //update the post with the user comment
-    post && post.comments.push({ id, content, status });
+    post && post.comments.push({ id, content, status })
   }
   //if the 'type' is 'CommentUpdated'
-  if (type === "CommentUpdated") {
-    const { id, content, postId, status } = data;
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data
     //get the post with the postId from DB
-    const post = posts[postId];
+    const post = posts[postId]
     //find the commet tfrom the comments array
-    const comment = post.comments.find((comment) => comment.id === id);
+    const comment = post.comments.find((comment) => comment.id === id)
     //update the status
-    comment.status = status;
-    comment.content = content;
+    comment.status = status
+    comment.content = content
     //update the content
   }
-};
+}
