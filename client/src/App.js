cc238380ff4d70d3e9a9ebf921e1e3e2c6d9d1b8
Adetola Alexander Agunbiade <30411005+myportfolios@users.noreply.github.com>
2023-12-08 14:08:03 -0500

    Added exclamation to title to check if skaffold works. it does

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import CreatePost from './CreatePost';
-import PostsList from './PostsList';
-import './App.scss';
+import CreatePost from './CreatePost'
+import PostsList from './PostsList'
+import './App.scss'
 
 function App() {
   return (
-    <div className="container">
-      <h1>Create Post</h1>
+    <div className='container'>
+      <h1>Create Post!!</h1>
       <CreatePost />
       <hr />
       <h1>Posts</h1>
       <PostsList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
